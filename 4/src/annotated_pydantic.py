9dde993bfd3cad591468e221d1706c34b4ef875e
t-eguchi <t-eguchi@nssys.co.jp>
2024-10-22 19:13:33 +0900

    コミット

--- 4/src/annotated_pydantic.py
+++ 4/src/annotated_pydantic.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel, Field
 
 class Foo(BaseModel):
     positive: Annotated[int, Field(gt=0)]
+     # エラーが発生しうるため、非推奨とのこと
+     # （筆者の環境では問題なく動く）
     non_negative: Optional[int] = Field(ge=0)
 
 foo = Foo(
